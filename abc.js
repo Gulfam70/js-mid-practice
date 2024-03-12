6a32b519c3d298e48e97dd1a42428d499fab18d1
gulman70 <gulfamchanna70@gmail.com>
2024-03-12 05:47:31 -0700

    arrays in js

--- abc.js
+++ abc.js
@@ -1,12 +1,14 @@
 console.log("working");
-let person={
-    name : "Gulfam",
-     age:21,
-    isAllowed : false,
-        
-};
-console.log(person);
-person.name="hamza";
-console.log(person);
-person.isAllowed=true;
-console.log(person);
+let colors=['blue','pink','red'];
+colors[3]= 'green';
+console.log(colors);
+
+console.log(colors.length);  
+
+// new array
+let pc_specs=['i5 6400', 'asus b250', 'hyper X 16gigs', 'RX 5500XT','BUNCH OF RGBS'];
+console.log(pc_specs);
+let peripherals =['Logitech G502', 'BLUE SWITCHES', 'HYPER X CLOUD II'];
+console.log(peripherals);
+console.log("type of array is "+ typeof(peripherals));
+console.log("total periphirals are "+ peripherals.length);
